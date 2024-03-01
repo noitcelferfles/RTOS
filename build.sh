1a0f449cdd138791778d8a259c4e8a72038bda36
Tian Xia <tian.xia0627@gmail.com>
2024-03-01 19:41:04 +0000

    QOL changes

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
+#!/bin/sh
+
+# Required packages:
+#   g++-arm-none-eabi
+#   meson
+#   ninja
+
 # Commands for building the project
 meson setup --cross-file ./compilation_setup.txt build --reconfig
 cd ./build
